02092c62bd545b15b1930e45e389813346cdd285
mrheza <m.rheza69@gmail.com>
2020-03-24 13:39:04 +0700

    feat: add readme

---
+++ README.md
@@ -0,0 +1,49 @@
+# Description
+
+This is task manager CLI using nodejs, pouchdb for local database and couchdb for remote database. This is offline first app.
+
+# Requirements
+
+Node Js 12 (recommended).
+
+# Installation
+
+``` bash
+# install dependencies
+npm install
+
+# will create a symlink in the global folder to run command app globally
+npm link
+```
+
+# Command List
+
+Show help menu for a command
+```
+$ task -h
+```
+
+Show package version
+```
+$ task -v
+```
+
+Show task list
+```
+$ task list
+```
+
+Set task status to complete
+```
+$ task update c <taskid>
+```
+
+Set task status to incomplete
+```
+$ task update i <taskid>
+```
+
+Upload unsync data from localdb to remotedb
+```
+$ task upload
+```
--- src/help.js
+++ src/help.js
@@ -3,11 +3,11 @@ import chalk from 'chalk';
 const menus = {
   main: `
 ${chalk.greenBright('task [command] <options>')}
-  ${chalk.blueBright('list')} ............... show task list
-  ${chalk.blueBright('upload')} ............. upload unsync data from locatedb to remote db
+  ${chalk.blueBright('list')} ................ show task list
+  ${chalk.blueBright('upload')} ............. upload unsync data from localdb to remote db
   ${chalk.blueBright('update')}.............. set task status to complete/incomplete; Options: c/i <taskid>
-  ${chalk.blueBright('version')} ............ show package version
-  ${chalk.blueBright('help')} ............... show help menu for a command
+  ${chalk.blueBright('version, v')} .............. show package version
+  ${chalk.blueBright('help, h')} ................. show help menu for a command
 `,
 }
 
